0c34d5388e2fafd6136e43e9d26512e5232b5c71
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-05-20 14:13:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
 import scratchattach as scratch3
-import os
-session = scratch3.login("Iegend-", "kitloin")
-conn = session.connect_cloud("853976819") #replace with your project id
 
-client = scratch3.CloudRequests(conn)
-
-@client.request
-def foo(argument1):
-    print(f"server requested to run {argument1}")
-    cmd = argument1
-    return os.system(cmd)
-
-client.run()
+session = scratch3.login("iegend-", "kitloin!@")
+print(session.session_id)
